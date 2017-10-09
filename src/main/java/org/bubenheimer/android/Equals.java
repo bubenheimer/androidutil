65564eec694ad42d15a217d8db5dc2384ca1a67a
Uli Bubenheimer <bubenheimer@users.noreply.github.com>
2017-10-09 21:33:37 +0200

    Upgrade libraries

--- src/main/java/org/bubenheimer/android/Equals.java
+++ src/main/java/org/bubenheimer/android/Equals.java
@@ -21,11 +21,6 @@ import org.bubenheimer.util.Uninstantiable;
 
 @SuppressWarnings("unused")
 public final class Equals extends Uninstantiable {
-    //TODO replace with Objects.equals() once I can use Java 7 APIs
-    public static boolean equals(final Object o1, final Object o2) {
-        return o1 == o2 || o1 != null && o1.equals(o2);
-    }
-
     public static boolean nanSafeEquals(final float f1, final float f2) {
         return f1 == f2 || f1 != f1 && f2 != f2;
     }
