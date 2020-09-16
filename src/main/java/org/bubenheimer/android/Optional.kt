84a27f7682de56872452f4697e50b3f88dd5ca19
Uli Bubenheimer <bubenheimer@users.noreply.github.com>
2020-09-15 23:14:56 -0500

    Refine type variance

--- src/main/java/org/bubenheimer/android/Optional.kt
+++ src/main/java/org/bubenheimer/android/Optional.kt
@@ -6,33 +6,34 @@ package org.bubenheimer.android
 
 import androidx.annotation.CheckResult
 
-public sealed class Optional<out T> {
-    public class Some<T>(public val value: T) : Optional<T>()
+public sealed class Optional<out T : Any> {
+    public class Some<out T : Any>(public val value: T) : Optional<T>()
     public object Empty : Optional<Nothing>()
 
     public companion object {
         @CheckResult
-        public fun <T> ofNullable(value: T?): Optional<T> = value?.let {
+        public fun <T : Any> ofNullable(value: T?): Optional<T> = value?.let {
             Some(value)
         } ?: Empty
 
         @CheckResult
-        public fun <T> of(value: T): Optional<T> =
+        public fun <T : Any> of(value: T): Optional<T> =
             Some(value)
     }
 }
 
 @get:CheckResult
-public val <T> Optional<T>.isPresent: Boolean get() = this is Optional.Some
+public val <T : Any> Optional<T>.isPresent: Boolean
+    get() = this is Optional.Some
 
 @CheckResult
-public fun <T> Optional<T>.orElse(other: T?): T? = when (this) {
+public fun <T : Any> Optional<T>.orElse(other: T?): T? = when (this) {
     is Optional.Some -> value
     is Optional.Empty -> other
 }
 
 @CheckResult
-public fun <T> Optional<T>.get(): T = when (this) {
+public fun <T : Any> Optional<T>.get(): T = when (this) {
     is Optional.Some -> value
     is Optional.Empty -> throw NoSuchElementException()
 }
