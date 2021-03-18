8197f2cd73c548a4dafdef10b3985229e0bed940
Uli Bubenheimer <bubenheimer@users.noreply.github.com>
2021-03-17 23:19:52 -0500

    Add stateFlowDelegate to SavedStateDelegate

--- src/main/java/org/bubenheimer/android/app/SavedStateDelegate.kt
+++ src/main/java/org/bubenheimer/android/app/SavedStateDelegate.kt
@@ -4,8 +4,11 @@
 
 package org.bubenheimer.android.app
 
+import android.os.Bundle
+import androidx.core.os.bundleOf
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
@@ -22,6 +25,47 @@ public class SavedStateDelegate public constructor(private val state: SavedState
     }
 }
 
+public val SavedStateHandle.stateFlowDelegate: SavedStateFlowDelegate
+    get() = SavedStateFlowDelegate(this)
+
+public class SavedStateFlowDelegate public constructor(private val state: SavedStateHandle) {
+    public operator fun <T : Any?> getValue(thisRef: Any?, property: KProperty<*>):
+            MutableStateFlow<T> {
+        @Suppress("UNCHECKED_CAST")
+        val flow = MutableStateFlow(state.get<Bundle>(property.name)?.get(INNER_KEY) as T)
+
+        state.setSavedStateProvider(property.name) { bundleOf(INNER_KEY to flow.value) }
+        return flow
+    }
+
+    private companion object {
+        private const val INNER_KEY: String = "key"
+    }
+}
+
+public fun <T : Any?> SavedStateHandle.stateFlowDelegate(initialValue: T):
+        ReadOnlyProperty<Any?, MutableStateFlow<T>> =
+    SavedStateStateFlowInitialDelegate(this, initialValue)
+
+private class SavedStateStateFlowInitialDelegate<T : Any?> constructor(
+    private val state: SavedStateHandle,
+    private val initialValue: T
+) : ReadOnlyProperty<Any?, MutableStateFlow<T>> {
+    override operator fun getValue(thisRef: Any?, property: KProperty<*>): MutableStateFlow<T> {
+        val bundle: Bundle? = state.get(property.name)
+
+        @Suppress("UNCHECKED_CAST")
+        val flow = MutableStateFlow(if (bundle == null) initialValue else bundle[INNER_KEY] as T)
+
+        state.setSavedStateProvider(property.name) { bundleOf(INNER_KEY to flow.value) }
+        return flow
+    }
+
+    private companion object {
+        private const val INNER_KEY: String = "key"
+    }
+}
+
 public val SavedStateHandle.liveDataDelegate: SavedStateLiveDataDelegate
     get() = SavedStateLiveDataDelegate(this)
 
@@ -34,10 +78,10 @@ public fun <T : Any?> SavedStateHandle.liveDataDelegate(initialValue: T):
         ReadOnlyProperty<Any?, MutableLiveData<T>> =
     SavedStateLiveDataInitialDelegate(this, initialValue)
 
-private class SavedStateLiveDataInitialDelegate<T : Any?> internal constructor(
+private class SavedStateLiveDataInitialDelegate<T : Any?> constructor(
     private val state: SavedStateHandle,
     private val initialValue: T
 ) : ReadOnlyProperty<Any?, MutableLiveData<T>> {
-    override operator fun getValue(thisRef: Any?, property: KProperty<*>):
-            MutableLiveData<T> = state.getLiveData(property.name, initialValue)
+    override operator fun getValue(thisRef: Any?, property: KProperty<*>): MutableLiveData<T> =
+        state.getLiveData(property.name, initialValue)
 }
