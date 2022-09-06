11d0f024a247c6a575d7ffd6b26dd2a10b9f88e7
Uli Bubenheimer <bubenheimer@users.noreply.github.com>
2022-09-05 22:08:11 -0400

    Remove android preferences and fragment support

--- src/main/java/org/bubenheimer/android/ui/UIUtil.kt
+++ src/main/java/org/bubenheimer/android/ui/UIUtil.kt
@@ -19,18 +19,6 @@ package org.bubenheimer.android.ui
 
 import android.content.Context
 import android.widget.Toast
-import androidx.fragment.app.Fragment
-
-/**
- * Utility method to retrieve whatever is the parent of a Fragment, whether it's
- * another Fragment or an Activity (or something else?). This is useful, for example,
- * for DialogFragments, which may hang off an Activity or another Fragment; whatever the
- * parent is can implement an interface to receive results.
- *
- * @return the parent Fragment if it exists, otherwise the non-null Context (typically an
- * Activity).
- */
-public val Fragment.parent: Any get() = parentFragment ?: requireContext()
 
 public fun Context.appToast(text: CharSequence, duration: Int): Toast =
     Toast.makeText(applicationContext, text, duration)
