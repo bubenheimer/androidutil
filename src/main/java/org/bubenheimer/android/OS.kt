ae3fe84febb61d11d3a6b1b5ba794ff7fd406c11
Uli Bubenheimer <bubenheimer@users.noreply.github.com>
2022-09-05 22:25:17 -0400

    Replace opt-in build option

--- src/main/java/org/bubenheimer/android/OS.kt
+++ src/main/java/org/bubenheimer/android/OS.kt
@@ -18,9 +18,11 @@
 package org.bubenheimer.android
 
 import android.os.Parcel
+import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
+@OptIn(ExperimentalContracts::class)
 public inline fun <T> withParcel(block: (Parcel) -> T): T {
     contract { callsInPlace(block, InvocationKind.EXACTLY_ONCE) }
 
